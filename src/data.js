53d56a6cbc3dfdf4769db85bcbe6aed7c96b22f4
Lee Dong Cheol <ehdcjf4874@daum.net>
2022-08-25 23:24:56 +0900

    assets 파일의 필요없는 폴더 삭제 및 폴더 이름 변경

--- src/data.js
+++ src/data.js
@@ -1,16 +1,7 @@
 import {fundamentalsCollection} from "./data/fundamentals.js";
 import {webcomponentsCollection} from "./data/web-components.js";
 import {accessibilityCollection} from "./data/accessibility.js";
-import {pwaCollection} from "./data/pwa.js";
-import {architectureCollection} from "./data/architecture-and-paradigmes.js";
-import {buildtoolsCollection} from "./data/build-tools.js";
-import {testingCollection} from "./data/testing.js";
-import {frameworksAndLibrariesCollection} from "./data/frameworks-and-libraries.js";
-import {modernCollection} from "./data/modern.js";
-import {teamCollaborationCollection} from "./data/team-collaboration.js";
-import {algorithmsCollection} from "./data/algorithms.js";
-import {designAndUXCollection} from "./data/design-and-ux.js";
-import {databasesCollection} from "./data/databases.js";
+
 
 /**
  * All available collections.
@@ -19,14 +10,4 @@ export const collections = [
 	fundamentalsCollection,
 	accessibilityCollection,
 	webcomponentsCollection,
-	pwaCollection,
-	buildtoolsCollection,
-	frameworksAndLibrariesCollection,
-	testingCollection,
-	architectureCollection,
-	teamCollaborationCollection,
-	designAndUXCollection,
-	modernCollection,
-	algorithmsCollection,
-	databasesCollection,
 ];
