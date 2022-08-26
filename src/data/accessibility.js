76b417e7e1ac40bf54d5728c4c31782604aa3d2b
tlans21 <cjstlans21@naver.com>
2022-08-26 16:06:09 +0900

    [FEAT:자료구조 틀 생성]#39

--- src/data/accessibility.js
+++ src/data/accessibility.js
@@ -178,6 +178,156 @@ export const accessibilityCollection = {
 					]
 				}
 			]
+		},
+		{
+			name: "중급 자료구조",
+			skills: [
+				{	name: "시뮬레이션",
+					description: {
+						text: "시뮬레이션 문제입니다.",
+						links: [
+							["난이도 - 중, ZOAC 3", "https://www.acmicpc.net/problem/20436"],
+							["난이도 - 중, 지구 온난화", "https://www.acmicpc.net/problem/5212"],
+							["난이도 - 상, 톱니바퀴", "https://www.acmicpc.net/problem/14891"],
+							["난이도 - 상, 감시", "https://www.acmicpc.net/problem/15683"]
+						]
+					}
+				},
+				{
+					name: "투 포인터",
+					description: {
+						text: "Learn what the CSS box model means.",
+						links: [
+							["난이도 - 중, 배열 합치기", "https://www.acmicpc.net/problem/11728"],
+							["난이도 - 상, 두 용액", "https://www.acmicpc.net/problem/2470"],
+							["난이도 - 중상, 겹치는건 싫어", "https://www.acmicpc.net/problem/20922"],
+						]
+					},
+				},
+				/*{
+					name: "Overflow",
+					description: {
+						text: "Learn about what happens when there is too much content to be contained comfortably inside a box and how to manage it.",
+						links: [
+							["MDN - Overflowing content", "https://developer.mozilla.org/en-US/docs/Learn/CSS/Building_blocks/Overflowing_content"],
+							["W3Schools - CSS Layout: Overflow", "https://www.w3schools.com/css/css_overflow.asp"],
+							["Overflow In CSS", "https://ishadeed.com/article/overflow-css/"]
+						]
+					},
+				},*/
+				{
+					name: "분할 정복",
+					description: {
+						text: "분할 정복 문제입니다.",
+						links: [
+							["난이도 - 중, 색종이 만들기", "https://www.acmicpc.net/problem/2630"],
+							["난이도 - 중, 쿼드 트리", "https://www.acmicpc.net/problem/1992"],
+							["난이도 - 중, Z", "https://www.acmicpc.net/problem/1074"],
+						]
+					},
+				},
+				{
+					name: "문자열",
+					description: {
+						text: "문자열 문제입니다.",
+						links: [
+							["난이도 - 하, 경고", "https://www.acmicpc.net/problem/3029"],
+							["난이도 - 중, 단어 정렬", "https://www.acmicpc.net/problem/1181"],
+							["난이도 - 중, 비밀번호 발음하기", "https://www.acmicpc.net/problem/4659"]
+						]
+					},
+				},
+				{
+					name: "누적합",
+					description: {
+						text: "누적합 문제입니다.",
+						links: [
+							["난이도 - 하, 2차원 배열의 합", "https://www.acmicpc.net/problem/2167"],
+							["난이도 - 중, 귀찮아 (SIB)", "https://www.acmicpc.net/problem/14929"],
+							["난이도 - 중, 점수 따먹기", "https://www.acmicpc.net/problem/1749"],
+							["난이도 - 상, 나누기", "https://www.acmicpc.net/problem/21757"],
+						]
+					},
+				}
+			]
+		},
+		{
+			name: "고급 자료구조",
+			skills: [
+				{	
+					name: "백트레킹",
+					description: {
+						text: "백트레킹 기법을 사용한 문제",
+						links: [
+							["난이도 - 중, N과 M (1)", "https://www.acmicpc.net/problem/15649"],
+							["난이도 - 상, 줄어드는 수", "https://www.acmicpc.net/problem/1174"],
+							["난이도 - 상, N-Queen", "https://www.acmicpc.net/problem/9663"],
+							/* ["You might not need jQuery", "http://youmightnotneedjquery.com/"],*/
+						]
+					}
+				},
+				{
+					name: "다익스트라 알고리즘",
+					description: {
+						text: "다익스트라 알고리즘",
+						links: [
+							["난이도 - 상, 특정 거리의 도시 찾기", "https://www.acmicpc.net/problem/18352"],
+							["난이도 - 상, 알고스팟", "https://www.acmicpc.net/problem/1261"],
+							["난이도 - 상, 최단 경로", "https://www.acmicpc.net/problem/1753"],
+							["난이도 - 상, 인터넷 설치", "https://www.acmicpc.net/problem/1800"],
+						]
+					},
+				},
+				{
+					name: "DFS",
+					description: {
+						text: "DFS, 깊이 우선탐색입니다.",
+						links: [
+							["난이도 - 상, 트리", "https://www.acmicpc.net/problem/1068"],
+							["난이도 - 중, 전쟁 - 전투", "https://www.acmicpc.net/problem/1303"],
+							["난이도 - 상, 트리의 지름", "https://www.acmicpc.net/problem/1167"],
+							["난이도 - 상, 노드 사이의 거리", "https://www.acmicpc.net/problem/1240"],
+							["난이도 - 상, 미친 로봇", "https://www.acmicpc.net/problem/1405"]
+						]
+					}
+				},
+				{
+					name: "BFS",
+					description: {
+						text: "BFS, 너비 우선탐색",
+						links: [
+							["난이도 - 상, 조각 움직이기", "https://www.acmicpc.net/problem/1035"],
+							["난이도 - 상, 교환", "https://www.acmicpc.net/problem/1039"],
+							["난이도 - 상, 달이 차오른다, 가자", "https://www.acmicpc.net/problem/1194"],
+							["난이도 - 상, 농장 관리", "https://www.acmicpc.net/problem/1245"],
+							["난이도 - 상, 노드사이의 거리", "https://www.acmicpc.net/problem/1240"],
+							["난이도 - 상, 소트게임", "https://www.acmicpc.net/problem/1327"],
+						]
+					}
+				},
+				{
+					name: "위상 정렬",
+					description: {
+						text: "위상 정렬 문제",
+						links: [
+							["난이도 - 상, 작업", "https://www.acmicpc.net/problem/2056"],
+							["난이도 - 상, 줄 세우기", "https://www.acmicpc.net/problem/2252"],
+							["난이도 - 상, 음악 프로그램", "https://www.acmicpc.net/problem/2623"],
+						]
+					}
+				},
+				{
+					name: "플로이드 워셜",
+					description: {
+						text: "플로이드 워셜 문제입니다.",
+						links: [
+							["난이도 - 중, 친구", "https://www.acmicpc.net/problem/1058"],
+							["난이도 - 상, 역사", "https://www.acmicpc.net/problem/1613"],
+							["난이도 - 상, 운동", "https://www.acmicpc.net/problem/1956"],
+						]
+					}
+				}
+			]
 		}
 	]
 };
