5aae6041a179c05977811a69a70c32d5b544de7a
tlans21 <cjstlans21@naver.com>
2022-08-27 13:51:55 +0900

    [FEAT]: 고급 자료구조#38

--- src/data/accessibility.js
+++ src/data/accessibility.js
@@ -7,171 +7,57 @@ export const accessibilityCollection = {
 			skills: [
 				{
 		
-					name: "탐색",
+					name: "정렬",
 					description: {
-						text: "완전탐색을 이용한 자료구조 문제",
+						text: "정렬 참고 사이트",
 						links: [
-							["난이도 - 하, 최소 직사각형", "https://school.programmers.co.kr/learn/courses/30/lessons/86491"],
-						]
-					}
-				},
-
-				{
-					name: "Screen Readers",
-					description: {
-						text: "Learn about assistive technology such as screen readers that reads sections of the page aloud based on the current focus.",
-						links: [
-							["The A11Y Project - Screen Reader Myths", "https://a11yproject.com/posts/people-who-use-screen-readers-dont-use-javascript/"],
-							["The A11Y Project - NVDA Screen Reader", "https://a11yproject.com/posts/getting-started-with-nvda/"],
-							["The A11Y Project - OS X Voiceover", "https://a11yproject.com/posts/getting-started-with-voiceover/"],
-							["Webaim - Screen Reader Survey", "https://webaim.org/projects/screenreadersurvey7/"]
-						]
-					}
-				},
-				{
-					name: "Accessibility tree",
-					description: {
-						text: "Learn about the accessibility tree and how assistive technology uses it.",
-						links: [
-							["Google Devs - The Accessibility Tree", "https://developers.google.com/web/fundamentals/accessibility/semantics-builtin/the-accessibility-tree"]
+							["선택정렬, 삽입정렬, 버블정렬,  합병정렬, 퀵 정렬", "https://hsp1116.tistory.com/33"],
+							["[동영상]:병합정렬 설명영상", "https://www.youtube.com/watch?v=QAyl79dCO_k"],
+							["[동영상]:퀵 정렬 설명영상", "https://www.youtube.com/watch?v=7BDzle2n47c"]
 						]
 					},
 					skills: [
 						{
-							name: "ARIA",
+							name: "그리디 알고리즘",
 							description: {
-								text: "Learn how to use ARIA descriptions an labels to help assistive technology understanding your website.",
+								text: "그리디 알고리즘 참고 사이트",
 								links: [
-									["Google Devs - ARIA Labels", "https://developers.google.com/web/fundamentals/accessibility/semantics-aria/aria-labels-and-relationships"],
-									["Google Devs - Hiding and updating content", "https://developers.google.com/web/fundamentals/accessibility/semantics-aria/hiding-and-updating-content"],
-									["The A11Y Project - ARIA States", "https://a11yproject.com/posts/ARIA-states/"],
-									["The A11Y Project - ARIA Properties", "https://a11yproject.com/posts/ARIA-properties/"],
-								]
-							}
-						},
-						{
-							name: "Accessible HTML",
-							description: {
-								text: "Learn how to write HTML in such as way that assistive technology better understands it.",
-								links: [
-									["Google Devs - Introduction to Semantics", "https://developers.google.com/web/fundamentals/accessibility/semantics-builtin/"],
-									["MDN - Accessibility HTML", "https://developer.mozilla.org/en-US/docs/Learn/Accessibility/HTML"],
-									["Webaim - Skip navigation", "https://webaim.org/techniques/skipnav/"],
-									["w3c - Headings", "https://www.w3.org/WAI/tutorials/page-structure/headings/"],
-									["Freecodecamp - Semantic Elements", "https://guide.freecodecamp.org/html/html5-semantic-elements/"]
-								]
-							},
-							skills: [
-								{
-									name: "Alt text",
-									description: {
-										text: "Learn how to use the alt attribute to provide a useful text alternative to this image.",
-										links: [
-											["Google Devs - Text Alternatives for Images", "https://developers.google.com/web/fundamentals/accessibility/semantics-builtin/text-alternatives-for-images"],
-											["Webaim - Accessible Images", "https://webaim.org/techniques/images/"],
-											["Webaim - Alternative text", "https://webaim.org/techniques/alttext/"],
-										]
-									}
-								}
-							]
-						},
-						{
-							name: "Accessible CSS",
-							description: {
-								text: "Learn how to write CSS in such as way that assistive technology better understands it.",
-								links: [
-									["Webaim - CSS", "https://webaim.org/techniques/css/"]
-								]
-							}
-						},
-					]
-				},
-				{
-					name: "Accessible forms",
-					description: {
-						text: "Learn how to build accessible forms that makes it usable to as many people as possible.",
-						links: [
-							["Webaim - Forms", "https://webaim.org/techniques/forms/"],
-							["Itnext - Form Accessibility Gudie", "https://itnext.io/form-accessibility-a-practical-guide-4062b7e2dd14"]
-						]
-					}
-				},
-				{
-					name: "UI States",
-					description: {
-						text: "Learn how to make the state of each UI element clear.",
-						links: [
-							["Google Devs - Accessible Styles", "https://developers.google.com/web/fundamentals/accessibility/accessible-styles"]
-						]
-					}
-				},
-				{
-					name: "Keyboard Accessibility",
-					description: {
-						text: "Learn how to make it easy for keyboard users to navigate your site.",
-						links: [
-							["Webaim - Keyboard", "https://webaim.org/techniques/keyboard/"],
-							["Smashing Magazine - Web with just a keyboard", "https://www.smashingmagazine.com/2018/07/web-with-just-a-keyboard/"]
-						]
-					},
-					skills: [
-						{
-							name: "Focus",
-							description: {
-								text: "Learn how to create a sensible tab order and how to make it easy for the users to locate the currently focused element.",
-								links: [
-									["web.dev - Control focus with tabindex", "https://web.dev/control-focus-with-tabindex/"],
-									["Google Devs - Focus", "https://developers.google.com/web/fundamentals/accessibility/focus/"],
-									["Google Devs - Dom Order Matters", "https://developers.google.com/web/fundamentals/accessibility/focus/dom-order-matters"],
-									["Google Devs - Using Tabindex", "https://developers.google.com/web/fundamentals/accessibility/focus/using-tabindex"],
-									["Google Devs - Track Focus", "https://developers.google.com/web/tools/chrome-devtools/accessibility/focus"],
-									["MDN - :focus-visible", "https://developer.mozilla.org/en-US/docs/Web/CSS/:focus-visible"],
-									["MDN - :focus-within", "https://developer.mozilla.org/en-US/docs/Web/CSS/:focus-within"]
+									["탐욕(그리디) 알고리즘", "https://www.zerocho.com/category/Algorithm/post/584ba5c9580277001862f188"],
+									["[동영상]:동빈나 - 그리디 알고리즘 ", "https://www.youtube.com/watch?v=PNPIk3hc6ic"]
 								]
 							}
 						}
 					]
 				},
+
 				{
-					name: "Accessible Colors",
+					name: "탐색",
 					description: {
-						text: "Learn how to select colors in such a way that users, including those with visual disabilities, can perceive the content on the page.",
+						text: "탐색 참고 사이트",
 						links: [
-							["Webaim - Contrast and Color", "https://webaim.org/articles/contrast/"],
-							["The A11Y Project - Check contrast", "https://a11yproject.com/posts/check-contrast-with-mobile-device/"],
-							["The A11Y Project - What is color contrast?", "https://a11yproject.com/posts/what-is-color-contrast/"],
-							["Designing accessible color systems", "https://stripe.com/en-dk/blog/accessible-color-systems"],
+							["완전탐색 알고리즘","https://hongjw1938.tistory.com/78"],
+							["선형, 이분 탐색 알고리즘", "https://nittaku.tistory.com/487"],
+							["해시 탐색법","https://bpblog.tistory.com/99"],
+							["[동영상]:브루트 포스 완전 탐색 알고리즘 3분만에 이해하기","https://www.youtube.com/watch?v=ZNa9-86uVEA"]
 						]
 					}
 				},
 				{
-					name: "Laws & Policies",
+					name: "다이나믹 프로그래밍",
 					description: {
-						text: "Learn about the governmental policies related to web accessibility.",
+						text: "다이나믹 프로그램 참고 사이트",
 						links: [
-							["W3C - Laws & Policies", "https://www.w3.org/WAI/policies/"],
-							["Webaim - Laws around the world", "https://webaim.org/articles/laws/world/"],
-							["Webaim - Section 508", "https://webaim.org/standards/508/checklist"],
-							["Webaim - WCAG 2 Checklist", "https://webaim.org/standards/wcag/checklist"],
-							["A11Y.dev - Lawsuits", "https://a11y.dev/470-digital-accessibility-cases-were-filed-in-q3-2018/"],
+							["동적 프로그래밍","https://www.zerocho.com/category/Algorithm/post/584b979a580277001862f182"]
+							["[동영상]:동빈나 - 다이나믹 프로그래밍","https://www.youtube.com/watch?v=FmXZG7D8nS4"]
 						]
 					},
 					skills: [
 						{
-							name: "Audits",
+							name: "구현",
 							description: {
-								text: "Learn how to conduct an accessibility review to improve the overall experience of using your site. Remember, good accessibility equals good UX!",
+								text: "구현 알고리즘 참고 사이트",
 								links: [
-									["web.dev - Accessibility audits", "https://web.dev/lighthouse-accessibility"],
-									["Google Devs - How to review", "https://developers.google.com/web/fundamentals/accessibility/how-to-review"],
-									["Webaim - Accessibility Tools", "https://webaim.org/articles/tools/"],
-									["Google Devs - Accessibility Reference", "https://developers.google.com/web/tools/chrome-devtools/accessibility/reference"],
-									["Webaim - Wave", "http://wave.webaim.org/"],
-									["Google Chrome - NoCoffee Extension", "https://chrome.google.com/webstore/detail/nocoffee/jjeeggmbnhckmgdhmgdckeigabjfbddl"],
-									["Digital A11Y - Accessibility Plugins", "https://www.digitala11y.com/accessibility-plug-ins-ie-chrome-firefox-browsers/"],
-									["Google Devs - Accessibility for teams", "https://developers.google.com/web/fundamentals/accessibility/a11y-for-teams"],
-									["WCAG 2.0 checklists", "https://www.wuhcag.com/wcag-checklist/"],
-									["How to Meet WCAG (Quick Reference)", "https://www.w3.org/WAI/WCAG21/quickref/"],
+									["구현 문제란?", "https://scshim.tistory.com/431"]
 								]
 							}
 						}
@@ -179,7 +65,7 @@ export const accessibilityCollection = {
 				}
 			]
 		},
-		{
+                          {
 			name: "중급 자료구조",
 			skills: [
 				{	name: "시뮬레이션",
@@ -257,73 +143,71 @@ export const accessibilityCollection = {
 				{	
 					name: "백트레킹",
 					description: {
-						text: "백트레킹 기법을 사용한 문제",
+						text: "백트레킹 알고리즘 참고사이트",
 						links: [
-							["난이도 - 중, N과 M (1)", "https://www.acmicpc.net/problem/15649"],
-							["난이도 - 상, 줄어드는 수", "https://www.acmicpc.net/problem/1174"],
-							["난이도 - 상, N-Queen", "https://www.acmicpc.net/problem/9663"],
-							/* ["You might not need jQuery", "http://youmightnotneedjquery.com/"],*/
+							["백트레킹 알고리즘 설명","https://hcr3066.tistory.com/27"],
+							["백트레킹 c++ 코드 구현", "https://velog.io/@yhames/C-%EB%B0%B1%ED%8A%B8%EB%9E%98%ED%82%B9-01-%EA%B8%B0%EB%B3%B8%EA%B0%9C%EB%85%90"],
+							["[동영상]: 백트레킹 설명 영상", "https://www.acmicpc.net/problem/9663"],
 						]
 					}
 				},
 				{
 					name: "다익스트라 알고리즘",
 					description: {
-						text: "다익스트라 알고리즘",
+						text: "다익스트라 알고리즘 참고사이트",
 						links: [
-							["난이도 - 상, 특정 거리의 도시 찾기", "https://www.acmicpc.net/problem/18352"],
-							["난이도 - 상, 알고스팟", "https://www.acmicpc.net/problem/1261"],
-							["난이도 - 상, 최단 경로", "https://www.acmicpc.net/problem/1753"],
-							["난이도 - 상, 인터넷 설치", "https://www.acmicpc.net/problem/1800"],
+							["다익스트라 알고리즘 python 설명", "https://justkode.kr/algorithm/python-dijkstra"],
+							["다익스트라 알고리즘 java 설명", "https://sskl660.tistory.com/59"],
+							["다익스트라 알고리즘 c++ 설명", "https://yabmoons.tistory.com/364"],
+							["[동영상]: 다익스트라 알고리즘 설명영상", "https://www.youtube.com/watch?v=611B-9zk2o4"],
 						]
 					},
 				},
 				{
 					name: "DFS",
 					description: {
-						text: "DFS, 깊이 우선탐색입니다.",
+						text: "DFS 알고리즘 참고사이트",
 						links: [
-							["난이도 - 상, 트리", "https://www.acmicpc.net/problem/1068"],
-							["난이도 - 중, 전쟁 - 전투", "https://www.acmicpc.net/problem/1303"],
-							["난이도 - 상, 트리의 지름", "https://www.acmicpc.net/problem/1167"],
-							["난이도 - 상, 노드 사이의 거리", "https://www.acmicpc.net/problem/1240"],
-							["난이도 - 상, 미친 로봇", "https://www.acmicpc.net/problem/1405"]
+							["DFS 알고리즘 python 설명", "https://data-marketing-bk.tistory.com/44"],
+							["DFS 알고리즘 java 설명", "https://codingnojam.tistory.com/44"],
+							["DFS 알고리즘 C++ 설명", "https://hongku.tistory.com/157"],
+							["[동영상]: 깊이 우선 탐색 알고리즘 설명영상", "https://www.youtube.com/watch?v=l0Rsu7dziws"]
 						]
 					}
 				},
 				{
 					name: "BFS",
 					description: {
-						text: "BFS, 너비 우선탐색",
+						text: "BFS알고리즘 참고사이트",
 						links: [
-							["난이도 - 상, 조각 움직이기", "https://www.acmicpc.net/problem/1035"],
-							["난이도 - 상, 교환", "https://www.acmicpc.net/problem/1039"],
-							["난이도 - 상, 달이 차오른다, 가자", "https://www.acmicpc.net/problem/1194"],
-							["난이도 - 상, 농장 관리", "https://www.acmicpc.net/problem/1245"],
-							["난이도 - 상, 노드사이의 거리", "https://www.acmicpc.net/problem/1240"],
-							["난이도 - 상, 소트게임", "https://www.acmicpc.net/problem/1327"],
+							["BFS 알고리즘 python 설명", "https://cyc1am3n.github.io/2019/04/26/bfs_dfs_with_python.html"],
+							["BFS 알고리즘 java 설명", "https://minhamina.tistory.com/36"],
+							["BFS 알고리즘 C++ 설명", "https://hongku.tistory.com/156"],
+							["[동영상]: 너비 우선 탐색 알고리즘 설명영상", "https://www.youtube.com/watch?v=66ZKz-FktXo"]
 						]
 					}
 				},
 				{
 					name: "위상 정렬",
 					description: {
-						text: "위상 정렬 문제",
+						text: "위상 정렬 알고리즘 참고사이트",
 						links: [
-							["난이도 - 상, 작업", "https://www.acmicpc.net/problem/2056"],
-							["난이도 - 상, 줄 세우기", "https://www.acmicpc.net/problem/2252"],
-							["난이도 - 상, 음악 프로그램", "https://www.acmicpc.net/problem/2623"],
+							["위상 정렬 알고리즘 python 설명", "https://velog.io/@younge/Python-%EA%B7%B8%EB%9E%98%ED%94%84-%EC%9C%84%EC%83%81-%EC%A0%95%EB%A0%AC-%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98"],
+							["위상 정렬 알고리즘 java 설명", "https://codingnojam.tistory.com/66"],
+							["위상 정렬 알고리즘 C++ 설명", "https://ssungkang.tistory.com/entry/Algorithm-%EC%9C%84%EC%83%81%EC%A0%95%EB%A0%AC"],
+							["[동영상]: 위상 정렬 알고리즘 설명영상", "https://www.youtube.com/watch?v=qzfeVeajuyc"]
 						]
 					}
 				},
 				{
 					name: "플로이드 워셜",
 					description: {
-						text: "플로이드 워셜 문제입니다.",
+						text: "플로이드 워셜 알고리즘 참고사이트",
 						links: [
-							["난이도 - 중, 친구", "https://www.acmicpc.net/problem/1058"],
-							["난이도 - 상, 역사", "https://www.acmicpc.net/problem/1613"],
-							["난이도 - 상, 운동", "https://www.acmicpc.net/problem/1956"],
+							["플로이드 워셜 알고리즘 python 설명", "https://it-garden.tistory.com/247"],
+							["플로이드 워셜 알고리즘 java 설명", "https://sskl660.tistory.com/61"],
+							["플로이드 워셜 알고리즘 C++ 설명", "https://ansohxxn.github.io/algorithm/floyd/"],
+							["[동영상]: 플로이드 워셜 알고리즘 설명영상", "https://www.youtube.com/watch?v=9574GHxCbKc"]
 						]
 					}
 				}
